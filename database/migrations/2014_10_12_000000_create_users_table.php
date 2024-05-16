c93d48a4c92b5004386d62c4ed769b95b5771280
110023 <110023>
2024-05-16 20:45:12 +0700

    ->nullable()

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,25 +12,26 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+            //
             $table->id();
             $table->string('name');
             $table->string('surname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('username', 255);
-            $table->string('password');
-            $table->string('userid', 255);
-            $table->string('role', 255);
+            $table->string('username', 255)->nullable();
+            $table->string('password')->nullable();
+            $table->string('userid', 255)->nullable();
+            $table->string('role', 255)->nullable();
             $table->rememberToken();
             $table->string('card_id', 13);
             $table->string('phone', 10);
-            $table->string('div2', 4);
-            $table->integer('groupid');
-            $table->string('agency', 10);
-            $table->string('PROV_ID', 10);
-            $table->string('AMP_ID', 10);
-            $table->string('edu_area_id', 10);
-            $table->string('file', 255);
+            $table->string('div2', 4)->nullable();
+            $table->integer('groupid')->nullable();
+            $table->string('agency', 10)->nullable();
+            $table->string('PROV_ID', 10)->nullable();
+            $table->string('AMP_ID', 10)->nullable();
+            $table->string('edu_area_id', 10)->nullable();
+            $table->string('file', 255)->nullable();
 
             $table->timestamps();
         });
