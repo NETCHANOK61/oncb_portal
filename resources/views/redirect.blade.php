afda0886a59dbc93aa3931b95e450e1550422bc1
110023 <110023>
2024-06-09 20:57:43 +0700

    ....

--- resources/views/redirect.blade.php
+++ resources/views/redirect.blade.php
@@ -8,7 +8,6 @@
 <body onload="document.forms['redirectForm'].submit()">
     <form name="redirectForm" action="{{ $redirectUrl }}" method="POST">
         @csrf
-        @dd($data)
         <input type="hidden" name="data" value="{{ $data }}">
         <noscript>
             <input type="submit" value="Click here if you are not redirected">
