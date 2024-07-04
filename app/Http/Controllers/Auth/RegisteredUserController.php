0fd920aec7514a8447d8848f5f11bb9064d53da3
110023 <110023>
2024-07-04 14:35:17 +0700

    check data

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -65,19 +65,35 @@ class RegisteredUserController extends Controller
 
     public function checkEmail(Request $request)
     {
-        $isAvailable = !User::where('email', $request->value)->exists();
+        $email = $request->value;
+
+        $userExists = User::where('email', $email)->exists();
+        $requestedUserExists = RequestedUser::where('email', $email)->exists();
+
+        $isAvailable = !$userExists && !$requestedUserExists;
+
         return response()->json(['isAvailable' => $isAvailable]);
     }
 
     public function checkCard_id(Request $request)
     {
-        $isAvailable = !User::where('card_id', $request->value)->exists();
+        $card_id = $request->value;
+
+        $userExists = !User::where('card_id', $card_id)->exists();
+        $requestedUserExists = RequestedUser::where('card_id', $card_id)->exists();
+
+        $isAvailable = !$userExists && !$requestedUserExists;
         return response()->json(['isAvailable' => $isAvailable]);
     }
 
-    public function checkUsername(Request $request)
+    public function checkUser_id(Request $request)
     {
-        $isAvailable = !User::where('username', $request->value)->exists();
+        $userid = $request->value;
+
+        $userExists = !User::where('userid', $userid)->exists();
+        $requestedUserExists = RequestedUser::where('userid', $userid)->exists();
+
+        $isAvailable = !$userExists && !$requestedUserExists;
         return response()->json(['isAvailable' => $isAvailable]);
     }
 
